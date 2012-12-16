383d055c600945d26cfe7f745844130ad2fd192c
Lukas Jørgensen <FuzzyVoid Studio>
2012-12-16 03:50:52 +0100

    IPS-AT update
    Fixed several visual bugs and added a lot of new features.
    (Right click, displays the options panel)

--- IPS-AT precompiled/creations/ParticleEffects.cs
+++ IPS-AT precompiled/creations/ParticleEffects.cs
@@ -4,3 +4,9 @@ datablock ParticleEffectData(TestEffect)
 	lifeTimeMS = 5000;
 };
 
+datablock ParticleEffectData(NoName)
+{
+	pEffect = "./ParticleEffects/NoName.pEffect";
+	lifeTimeMS = 5000;
+};
+
