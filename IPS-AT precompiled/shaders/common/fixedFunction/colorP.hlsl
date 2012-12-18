5a365c11b56c8cf9aed5bd3706d321f6faa5ffdf
Lukas Jørgensen <FuzzyVoid Studio>
2012-12-18 13:52:24 +0100

    restructure

--- IPS-AT precompiled/shaders/common/fixedFunction/colorP.hlsl
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------------------
-// Torque
-// Copyright GarageGames, LLC 2011
-//-----------------------------------------------------------------------------
-
-float4 main( float4 color_in : COLOR0, uniform sampler2D diffuseMap : register(S0) ) : COLOR0
-{
-   return color_in;
-}
